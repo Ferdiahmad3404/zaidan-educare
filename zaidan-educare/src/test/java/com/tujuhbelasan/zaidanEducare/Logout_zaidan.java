dbf395982aec84fe81ae028d82d72c7262268cca
ferdiahmad3404@gmail.com <ferdiahmad3404@gmail.com>
2025-05-24 15:43:19 +0700

    [Fix] Remove duplicate in Logout step definition file

--- zaidan-educare/src/test/java/com/tujuhbelasan/zaidanEducare/Logout_zaidan.java
+++ zaidan-educare/src/test/java/com/tujuhbelasan/zaidanEducare/Logout_zaidan.java
@@ -1,37 +1,26 @@
 package com.tujuhbelasan.zaidanEducare;
 
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 public class Logout_zaidan {
-    @Given("User login as bendahara")
-    public void user_login_as_bendahara() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-
-    @Given("User navigates to Dashboard page")
-    public void user_navigates_to_dashboard_page() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+    @And("User navigates to Dashboard page")
+    public void userNavigatesToDashboardPage() {
     }
 
     @When("User clicks on logout button")
-    public void user_clicks_on_logout_button() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+    public void userClicksOnLogoutButton() {
+        
     }
 
-    @When("Clicks on logout confirmation pop up button")
-    public void clicks_on_logout_confirmation_pop_up_button() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+    @And("Clicks on logout confirmation pop up button")
+    public void clicksOnLogoutConfirmationPopUpButton() {
+        
     }
 
     @Then("User should navigated to the Login page")
-    public void user_should_navigated_to_the_login_page() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+    public void userShouldNavigatedToTheLoginPage() {
     }
 }
